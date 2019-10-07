daeb98f9425097a28a8778fb4c0bb2603699e6d1
Franklin Waggoner <fcw1987@mac.com>
2019-10-06 23:51:24 -0700

    adds working js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
 
+var pingPong = function(numbers) {
+  var pingPongResults =[];
+  for (i=1; i<=numbers; i+=1){
+    if ((i%3 !=0) && (i%5 !=0) && (i%15 !=0)){
+      pingPongResults.push(i);
+    } else if ((i%15===0)){
+        pingPongResults.push("pingpong");
+    } else if (i%5===0){
+        pingPongResults.push("pong");
+    } else if (i%3===0){
+        pingPongResults.push("ping");
+      }
+    }
+
+  return pingPongResults;
+  }
 
 
 
 $(document).ready(function() {
-  $("form#pingpong").submit(function(event) {
+  $("form#userInputHere").submit(function(event) {
     event.preventDefault();
-    
+    $('#pingsPonged').empty();
+
+    var usersNumber = parseInt($("#number").val());
+    var outcomes = pingPong(usersNumber);
+        if (outcomes === undefined || outcomes ===null || (outcomes <=0)){
+    alert ("Enter a number greater than 0");
+    } else {
+        outcomes.forEach (function (numeral){
+        $("#pingsPonged").append("<li>" + numeral + "</li>");
+        })
+      }
     });
     });
